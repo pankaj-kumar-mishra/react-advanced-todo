a1e0cae06ce9b6553dc11300057ce1e4dda5f7d8
Pankaj Mishra <pankaj_mishra@epam.com>
2023-12-10 13:48:04 +0530

    todo container added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-// import './styles/general.scss'
-// import { Header } from './partials/Header/Header'
-// import TodosContainer from './containers/TodosContainer'
-
-// function App() {
-//   return (
-//     <>
-//       <Header />
-//       <div className="ml-auto mr-auto" style={{ width: '500px' }}>
-//         <TodosContainer />
-//       </div>
-//     </>
-//   )
-// }
-
-import SolidPrinciple from './solid'
+import './styles/general.scss'
+import { Header } from './partials/Header/Header'
+import TodosContainer from './containers/TodosContainer'
 
 function App() {
-  return <SolidPrinciple />
+  return (
+    <>
+      <Header />
+      <div className="ml-auto mr-auto" style={{ width: '500px' }}>
+        <TodosContainer />
+      </div>
+    </>
+  )
 }
+
+// import SolidPrinciple from './solid'
+
+// function App() {
+//   return <SolidPrinciple />
+// }
 
 export default App
